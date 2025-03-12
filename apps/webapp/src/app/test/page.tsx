b4e9ad198271829c0370feda48f249e5e0f3e589
ahmedfarid <ahmedfareed2025@gmail.com>
2025-03-12 15:31:10 +0200

    feat: init ui library

---
+++ apps/webapp/src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@dru/web-ui';
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>TestPage</h1>
+      <Button>Submit</Button>
+    </div>
+  );
+}
--- libs/web-ui/tailwind.config.js
+++ libs/web-ui/tailwind.config.js
@@ -3,13 +3,8 @@ const { join } = require('path');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ['class'],
   content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx}'
-    ),
-    join(__dirname, '../../libs/web-ui/src/**/*!(*.stories|*.spec).{ts,tsx}'),
+    join(__dirname, 'src/**/*!(*.stories|*.spec).{ts,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
@@ -48,11 +43,43 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        sidebar: {
+          DEFAULT: 'hsl(var(--sidebar-background))',
+          foreground: 'hsl(var(--sidebar-foreground))',
+          primary: 'hsl(var(--sidebar-primary))',
+          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+          accent: 'hsl(var(--sidebar-accent))',
+          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+          border: 'hsl(var(--sidebar-border))',
+          ring: 'hsl(var(--sidebar-ring))',
+        },
       },
       borderRadius: {
-        lg: `var(--radius)`,
-        md: `calc(var(--radius) - 2px)`,
+        lg: '`var(--radius)`',
+        md: '`calc(var(--radius) - 2px)`',
         sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: {
+            height: '0',
+          },
+          to: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+        },
+        'accordion-up': {
+          from: {
+            height: 'var(--radix-accordion-content-height)',
+          },
+          to: {
+            height: '0',
+          },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
